c235f2c2d8ec1b11d88206668d41964213ae5cd5
Vlas Laptev <vlaptev@vp.ru>
2016-10-14 23:44:02 +0600

    Completed basic functionality

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 var steps;
+var $_baseDiv;
 
 $(document).ready(function () {
     setDraggable($('.round'));
     droppable($('.tower'));
     steps = 0;
+    $_baseDiv = $(".base")[0];
 });
 
+function cancel(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+}
 
 function setDraggable(element) {
     $(element)
@@ -18,8 +25,57 @@ function setDraggable(element) {
         })
 }
 
-
-function canBeDragged(from, roundId) {
-    return $(from).children()[4].id == roundId;
+function droppable(element) {
+    $(element)
+        .bind('dragover', cancel)
+        .bind('drop', function (event) {
+            event.preventDefault();
+            var target = $(event.target);
+            if (!target.hasClass("tower")) {
+                target = $(target.parents(".tower"));
+            }
+            var discId = event.originalEvent.dataTransfer.getData("Text");
+            if (discId && canAppend(target, discId)) {
+                $("#count").text(++steps);
+                var disc = $("#" + discId);
+                disc.remove();
+                target.append(disc);
+                setDraggable(disc);
+            }
+            winCheck();
+            return false;
+        });
 }
 
+function winCheck() {
+    if ($("#tower-3").children().length == 5) {
+        $("#steps").append('<p>Победа</p>')
+    }
+}
+
+function canBeDragged(from, discId) {
+    return $(from).children().last().attr('id') == discId;
+}
+
+function canAppend(target, discId) {
+    var children = $(target).children();
+    return children.length == 1 || parseInt(children[1].id) >= parseInt(discId);
+}
+
+$(".restart").click(function () {
+    $("#steps").html('Ходы - <span id="count">0</span>');
+    steps = 0;
+    $(".tower").children().each(function () {
+        if (!$(this).hasClass("base")) {
+            $(this).detach().appendTo("#tower-1");
+        }
+    });
+
+    $("div[id*=_round]").sort(function (a, b) {
+        if (parseInt(a.id) > parseInt(b.id)) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }).appendTo("#tower-1");
+});
